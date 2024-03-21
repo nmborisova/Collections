7086784d8bc0f7304d5ddb4a12075073b50fda1c
n.m.borisova <K8sedrf@1q>
2024-03-21 10:06:04 +0200

    updates

--- src/bg/smg/Util.java
+++ src/bg/smg/Util.java
@@ -1,6 +1,12 @@
 package bg.smg;
 
+import java.util.Arrays;
+
 public class Util {
+    public static void main(String[] args) {
+        String s = "23 4 5 1 8 0 9 456";
+        Arrays.stream(getNumberValues(s)).forEach(e -> System.out.print(e+" "));
+    }
 
     public static boolean isNumeric(String strNum) {
         if (strNum == null) {
@@ -25,6 +31,16 @@ public class Util {
             }
         }
         return result;
+    }
+
+    public static char getOperation(String s){
+        switch (s) {
+            case "+" : return '+';
+            case "-" : return '-';
+            case "/" : return '/';
+            case "*" : return '*';
+        }
+        throw new UnsupportedOperationException("String is not supported operation");
     }
 
 }
